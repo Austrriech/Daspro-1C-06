695906300ab67265d96ed537fc6f566392ad3377
Advent Christopher <adventchristopher21@gmail.com>
2023-12-27 02:19:09 +0700

    P9 - LinearSearch06

--- src/P9_Array1/LinearSearch06.java
+++ src/P9_Array1/LinearSearch06.java
@@ -1,18 +1,37 @@
 package P9_Array1;
 
+import java.util.Scanner;
+
 public class LinearSearch06 {
     public static void main(String[] args) {
-        int[] arrayInt = {34, 18, 26, 48, 72, 20, 56, 63};
-        int key = 20;
+        int[] arrayInt;
+        int key, jumlahElemenArray;
         int hasil = 0;
+        boolean isFound = false;
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah elemen array: ");
+        jumlahElemenArray = sc.nextInt();
+
+        arrayInt = new int[jumlahElemenArray];
+        for (int i = 0; i < jumlahElemenArray; i++) {
+            System.out.print("Masukkan elemen array ke-" + i + " : ");
+            arrayInt[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan key yang ingin dicari: ");
+        key = sc.nextInt();
 
         for (int i = 0; i < arrayInt.length; i++) {
             if (arrayInt[i] == key) {
                 hasil = i;
+                isFound = true;
                 break;
             }
         }
 
-        System.out.println("Key ada dalam array pada posisi indeks ke-" + hasil);
+        if (isFound) System.out.println("Key ada di posisi indeks ke-" + hasil);
+        else System.out.println("Key tidak ditemukan");
     }
 }
